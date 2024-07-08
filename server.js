0faaf3b3c984599bed54a2aa895aa897afa90aeb
jamesgayflor <shekep1000@gmail.com>
2024-07-08 11:58:32 +0000

    Updates

---
+++ server.js
@@ -0,0 +1,36 @@
+// import
+const express = require('express');
+const server = express();
+const port = 3000;
+
+
+// sTATIC FILES
+server.use(express.static('public'));
+
+
+
+// Links
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/' + "home.html");
+})
+
+server.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/public/' + "home.html");
+})
+
+server.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/public/' + "about.html");
+})
+
+server.get('/skills', (req, res) => {
+    res.sendFile(__dirname + '/public/' + "skills.html");
+})
+
+server.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/public/' + "contact.html");
+})
+
+// Listen on port 3000
+server.listen(port, () => {
+    console.log(`Listening on port ${port}.`);
+})
