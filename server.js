4cebaf305dff64988318ed21109990f287bb5014
jamesgayflor <shekep1000@gmail.com>
2024-07-08 12:34:05 +0000

    Updates

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // import
 const express = require('express');
+const path = require('path');
 const server = express();
 const port = 3000;
 
 
 // sTATIC FILES
-server.use(express.static('public'));
+server.use(express.static(path.join(__dirname, 'public')));
 
 
 
